82509f1a166d8f6ecd5513a57b718a7c0337f43e
Thomas <thomas.schloesser@hotmail.com>
2024-11-05 12:32:21 +0000

    Added temporary function to start button and a text box which respons to said function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit
 import random
 class main_menu():
     def show_gui(self):
@@ -10,11 +10,18 @@ class main_menu():
         window.setWindowTitle('Main Menu')
 
         layout = QVBoxLayout()
+        
+        placeholder_text = QLineEdit(self.hello[0])
+        placeholder_text.setReadOnly(True)
+        placeholder_text.move(20,20)
+        placeholder_text.resize(200,40)
+        layout.addWidget(placeholder_text)
 
         start_button = QPushButton('Start')
         start_button.resize(500,500)
+        start_button.clicked.connect(lambda: placeholder_text.setText(random.choice(self.hello)))
         layout.addWidget(start_button)
-        start_button.clicked.connect(self.start_prompt)
+
         
         exit_button = QPushButton('Exit')
         exit_button.resize(500,500)
@@ -27,8 +34,6 @@ class main_menu():
 
         app.exec_()
         
-    def start_prompt(self):
-        self.text.setText(random.choice(self.hello))
         
 if __name__ == '__main__':
     main_menu().show_gui()

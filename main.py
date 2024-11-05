a8a39e3a38e973bb3ac4d6922870f95045986846
Thomas <thomas.schloesser@hotmail.com>
2024-11-05 12:20:59 +0000

    Added exit button and functionality to start button

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout
-
+import random
 class main_menu():
     def show_gui(self):
         app = QApplication([])
+
+        self.hello = ["Yo","Wassup","Hello"]
 
         window = QWidget()
         window.setWindowTitle('Main Menu')
@@ -10,13 +12,23 @@ class main_menu():
         layout = QVBoxLayout()
 
         start_button = QPushButton('Start')
+        start_button.resize(500,500)
         layout.addWidget(start_button)
+        start_button.clicked.connect(self.start_prompt)
+        
+        exit_button = QPushButton('Exit')
+        exit_button.resize(500,500)
+        exit_button.clicked.connect(app.quit)
+        layout.addWidget(exit_button)
         window.resize(1280, 960)
         
         window.setLayout(layout)
         window.show()
 
         app.exec_()
+        
+    def start_prompt(self):
+        self.text.setText(random.choice(self.hello))
         
 if __name__ == '__main__':
     main_menu().show_gui()

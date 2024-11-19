f9c5918fd69b6a6a8af705b8efe9007baf897b4e
Thomas Schloesser <thoams.schloesser@hotmail.com>
2024-11-19 19:54:23 +0000

    test

--- main.py
+++ main.py
@@ -8,43 +8,92 @@ pygame.init()
 
 pygame.display.set_caption("Platformer")
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1280, 960
 FPS = 60
 PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+class Player(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+    
+    def __init__(self, x, y ,width, height):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+        
+    def move_left(self, vel):
+        self.x_vel = -vel#
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+        
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+            
+    def loop(self, fps):
+        self.move(self.x_vel, self.y_vel)
+        
+    def draw(self, window):
+        pygame.draw.rect(window, self.COLOR, self.rect)
+
+
 def get_background(name):
-    image = pygame.image.load(join("assets", "backgrounds", name))
+    image = pygame.image.load(join("assets", "Background", name))
     _, _, width, height = image.get_rect()
     tiles = []
     
     for i in range(WIDTH // width + 1):
         for j in range(HEIGHT // height + 1):
-            pos = [i * width, j * height]
+            pos = (i * width, j * height)
             tiles.append(pos)
             
-    return image, tiles
+    return tiles, image
 
-def draw(window, background, bg_image):
+def draw(window, background, bg_image, player):
     for tile in background:
         window.blit(bg_image, tile)
+        
+    player.draw(window)
+        
+    pygame.display.update()
+    
+def handle_movement(keys, player):
+    key = pygame.key.get_pressed()
+    player.x_vel = 0
+    
+    if key[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+    elif key[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
+    else:
+        player.x_vel = 0
 
 def main(window):
     clock = pygame.time.Clock()
-    background, bg_image = get_background("Purple.png")
+    tiles, bg_image = get_background("Purple.png")
+    
+    player = Player(100, 100, 50, 50)
     
     run = True
     while run:
         clock.tick(FPS)
+        player.loop(FPS)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
             
-        draw(window, background, bg_image)
+        draw(window, tiles, bg_image, player), 
             
     pygame.quit()
     quit()

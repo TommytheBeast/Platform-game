00e3868141c88571954c7a2addfb9c1dc6d620ce
Thomas Schloesser <thoams.schloesser@hotmail.com>
2024-11-19 22:03:07 +0000

    changed sprite locations and added sprite sheet loading

--- main.py
+++ main.py
@@ -9,19 +9,48 @@ pygame.init()
 pygame.display.set_caption("Platformer")
 
 WIDTH, HEIGHT = 1280, 960
-FPS = 60
+FPS = 240
 PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def flip_sprite(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+    
+    all_sprites = {}
+    
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+        
+        sprites = []
+        for i in range(sprite_sheet.get() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA)
+            rect = pygame.Rect((i * width, 0, width, height))
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+            
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip_sprite(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+            
+    return all_sprites
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
+    GRAVITY = 2
     
     def __init__(self, x, y ,width, height):
         self.rect = pygame.Rect(x, y, width, height)
         self.x_vel = 0
         self.y_vel = 0
         self.mask = None
+        self.direction = None
+        self.fall_count = 0
         
     def move(self, dx, dy):
         self.rect.x += dx
@@ -40,7 +69,9 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
             
     def loop(self, fps):
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
+        self.fall_count += 1
         
     def draw(self, window):
         pygame.draw.rect(window, self.COLOR, self.rect)
@@ -93,6 +124,8 @@ def main(window):
                 run = False
                 break
             
+        player.loop(FPS)
+        handle_movement(pygame.key.get_pressed(), player)
         draw(window, tiles, bg_image, player), 
             
     pygame.quit()
